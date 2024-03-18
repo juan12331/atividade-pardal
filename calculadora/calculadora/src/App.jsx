1b216cf58c1c9240f8f71655b995dd1c3da5d477
juan12331 <146020152+juan12331@users.noreply.github.com>
2024-03-18 17:05:45 -0300

    Add files via upload

---
+++ calculadora/calculadora/src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div id="tudo">
+    <div id="tela">
+      <div id="resultados">
+        <div id="resultado1">
+
+        </div>
+        <div id="resultador2">
+          <span>0</span>
+        </div>
+
+
+    <div id="button">
+
+        <button>AC</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>0</button>
+        <button>+</button>
+        <button>-</button>
+        <button>*</button>
+        <button>/</button>
+        <button>=</button>
+        </div>
+
+      </div>
+
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
